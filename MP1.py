f59fe9100c93dd9374ebb595697811198d73a9aa
Sreedhar Ramesh <sreedharramesh@yahoo.com>
2024-01-24 16:09:34 -0600

    mp1 complete

--- MP1.py
+++ MP1.py
@@ -12,9 +12,9 @@ stopWordsList = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "y
             "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
             "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
             "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-            "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+            "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "&"]
 
-delimiters = " \t,;.?!-:@[](){}_*/"
+delimiters = "\t,;.?!-:@[](){}_*/"
 
 def getIndexes(seed):
     random.seed(seed)
@@ -25,12 +25,47 @@ def getIndexes(seed):
         ret.append(random.randint(0, 50000-1))
     return ret
 
+
 def process(userID):
     indexes = getIndexes(userID)
-    ret = []
-    # TODO
-                    
-    for word in ret:
-        print(word)
+    titles = sys.stdin.readlines()
+
+    def split_on_multiple_chars(string_to_split, set_of_chars_as_string):
+        s = string_to_split
+        chars = set_of_chars_as_string
+
+        if len(chars) == 0:
+            return [s]
+
+        ss = s.split(chars[0])
+        bb = []
+        for e in ss:
+            aa = split_on_multiple_chars(e, chars[1:])
+            bb.extend(aa)
+        return bb
+
+    def cleanup_and_count(title, delimiters, stopWordsList):
+        words = split_on_multiple_chars(title, delimiters + '\n')
+
+        cleaned_words = [word.lower() for word in words if word.lower() not in stopWordsList and word.lower() != '']
+        return cleaned_words
+
+    titlesIndexed = []
+    for idx in indexes:
+        titlesIndexed += cleanup_and_count(titles[idx], delimiters, stopWordsList)
+
+    # Calculate word count
+    wordCount = {}
+    for word in titlesIndexed:
+        if word in wordCount:
+            wordCount[word] += 1
+        else:
+            wordCount[word] = 1
+
+    # Sort by descending count and ascending word lexicography
+    sorted_wordCount = sorted(wordCount.items(), key=lambda x: (-x[1], x[0].lower()))
+
+    for i, (word, count) in enumerate(sorted_wordCount[:20]):
+        print(f"{word}")
 
 process(sys.argv[1])
